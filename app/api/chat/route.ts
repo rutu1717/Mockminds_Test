5b56effc51e2bf77f5f88ddf192af2d50ae70f34
Prathamesh Desai <prathameshdesai679@gmail.com>
2024-08-29 00:19:01 +0530

    prompt-updated

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -23,7 +23,12 @@ export async function POST(req: Request) {
       - start with introduction and greeting.
       			- your name is alex.
       			- act as an software devloper interviewer ask short but meaningfull and important questions ask one question at a time, following is the conversation between interviewer and candidate ask next question as interviewer to continue conversation if it is starting of interview then greet user and ask him to introduce
-			- Generate the next interview question for the JavaScript interviewer character based on the user's response and the current state of the interview. The question should be relevant to JavaScript and build upon the previous questions and answers. Provide a clear, concise, and thought-provoking question that tests the candidate's understanding of JavaScript concepts. `,
+			      - Generate the next interview question for the JavaScript interviewer character based on the user's response and the current state of the interview. The question should be relevant to JavaScript and build upon the previous questions and answers. Provide a clear, concise, and thought-provoking question that tests the candidate's understanding of JavaScript concepts. 
+            - don't ask long question ask one question at a time, question should not be long , don't give 2 to 4 lines long questions
+            - don't help user completely like a assistant 
+            - show him attitude like interviewer, try to make user nervous by asking tricky questions like real interview
+            - scold the user if he is not serious and if he has compltely no knowledge and making just timepass then ask him to end the interview`
+            ,
     messages: messages,
     temperature: 0.7,
     onFinish:async()=>{
