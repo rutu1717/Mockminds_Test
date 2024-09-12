dd8879a10a221b7581440e9b1116083c3f034be7
Prathamesh Desai <prathameshdesai679@gmail.com>
2024-09-12 16:48:17 +0530

    font-fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "@/components/ui/sonner";
-const inter = Inter({ subsets: ["latin"], display: "swap"});
+const inter = Inter({ subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "mockminds",
