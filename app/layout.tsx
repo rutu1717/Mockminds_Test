fb1c14cd51ebe6758be82cb0564289ab26d0f782
Prathamesh Desai <prathameshdesai679@gmail.com>
2024-09-12 16:14:05 +0530

    font changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "@/components/ui/sonner";
-const inter = Inter({ subsets: ["latin"] });
+const sanserif = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "mockminds",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={sanserif.className}>
         <Providers>
           {children}
         </Providers>
